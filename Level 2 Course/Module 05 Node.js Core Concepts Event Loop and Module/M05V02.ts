702cfaf624ef7f3f1fa37f91cbfede4a93ee8255
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-13 00:32:28 +0600

    Frontend and Backend in browser server interaction for M05V02

---
+++ Level 2 Course/Module 05 Node.js Core Concepts Event Loop and Module/M05V02.ts
@@ -0,0 +1,21 @@
+// Frontend vs Backend Essentials
+
+// Frontend- UI designed using framework: React, Veu, Angular.js, Next.js
+//           for browser, everything is HTML, CSS, Js
+
+// Backend- Serverbased, language to run server- python, php, rails, java
+//                                              node,js - js runtime environment
+
+// Static website vs Dynamic Website
+
+// Dynamic Website- Templace engine based and API based
+
+// Template Engine Based- Express.js- Server Side Rendering
+
+// API Based- Json data from Server - Server Rendering in Browser- Client Side Rendering
+
+// Single Server can be used on different platform- Browser, Mobile, Desktop 
+//                                                  it uses Json String to render
+
+
+
