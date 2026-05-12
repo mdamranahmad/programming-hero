f6388ef67f1f973a3f9e16f7744e13c2a50b08dc
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-13 00:53:33 +0600

    Components of Node.js M05V04

---
+++ Level 2 Course/Module 05 Node.js Core Concepts Event Loop and Module/M05V04.ts
@@ -0,0 +1,16 @@
+// How Node.js revolutionized the server landscape
+
+// Node.js dependencies - built with V8 Engine: Written with js, c++, Libuv
+
+// V8 Engine- most important - conver js to machine language (binary)
+
+// Libuv-   open source library written in c++
+//      - allow node to access Computer OS, File System, Networking 
+//      -- play two important part - Event loop - Execute callback function -   manage request
+//                                              - Networking input output   -   
+//                                  Thread Pool - CPU intensive             
+//                                              - File Access
+//                                              - File Compression
+//                                              - Cryptography
+
+
