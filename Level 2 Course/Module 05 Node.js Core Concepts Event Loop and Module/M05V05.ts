438f4b58698901dd8f559f5299db3d5e7f028566
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-13 01:32:25 +0600

    Architecture of node.js M05V05

---
+++ Level 2 Course/Module 05 Node.js Core Concepts Event Loop and Module/M05V05.ts
@@ -0,0 +1,21 @@
+// Event Driven Architecture of Node.js
+
+// Node.js  -   Event Emitter
+//          -   Event Listener
+//          -   Callback Function
+//          -   all of these are part of event loop
+
+// Process  -   
+
+// Threads  -   unit of execution   -   single thread and multiple thread
+
+// Node.js Process  -   create a environment for js
+//                  -   node.js single threaded
+//                      -   Initialize progromme
+//                      -   Execute top level code
+//                      -   Required module
+//                      -   Register callback events
+//                  -   transfer to thread pool when gets pressurized
+//                  -   
+
+// Event Loop       -   Heart of node.js
