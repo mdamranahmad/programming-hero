0c67653ea19d22af373187ce1e09c5db803319a4
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-13 00:44:37 +0600

    Introduction to Node.js M05V03

---
+++ Level 2 Course/Module 05 Node.js Core Concepts Event Loop and Module/M05V03.ts
@@ -0,0 +1,24 @@
+// Tracing where everything began
+
+// HTML- Build Core Structure
+// CSS- Add Style to that structure
+// Js- Adds Functionality
+
+// initially, we can built js on browser, not on server
+
+// node.js- run on v8 engine
+
+// node.js modules- Operating System Module os  - in built
+//                - File System Module fs
+//                - Path Module path
+//                - HTTP Module http
+//                - URL Module url
+//                - Utilities Module util
+
+// Node.js - Runs js on serverside
+//         - Scalable Backend App
+//         - Single threaded, non-blocking input output- can perform multiple task at a time
+//         - data intensive, streaming application
+
+// Node.js - CPU intensive - to resolve, worker threads can be used
+//         - 
