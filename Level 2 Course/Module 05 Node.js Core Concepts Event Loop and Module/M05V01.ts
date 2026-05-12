9073b3ab17224d5161dac50852f5cce91558c51a
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-12 23:37:57 +0600

    Introduction to browser-server interaction M05V01

---
+++ Level 2 Course/Module 05 Node.js Core Concepts Event Loop and Module/M05V01.ts
@@ -0,0 +1,40 @@
+// How the web works from scratch
+// Request Response Mode - Browser sends request, servers send response according to the request
+// Client Server Architecture - where client and server are separate system for software
+
+// each URL contains three part- protocol://domain-name/resource
+
+// URL is mask for IP Address
+
+// DNS Server- converts the IP Address into URL
+
+// Browser sends the URL to DNS, and DNS sends back IP Address
+
+// Once browser got the IP Address, it creates an TCP/IP socket connection between Client and Server
+
+// Browser sends HTTP Request to server
+
+// Server sends back HTTP response to Client
+
+// What does HTTP request contain?
+
+// HTTP Request- Method: GET, POST, PATCH, PUT, DELETE
+//             - Request Headers: Host: Domain name
+//                                Accept-Encoding: gzip, deflate, br. zstd
+
+// Request Body- 
+
+// HTTP Response- Status Code: 200 (success)
+//                Response Headers: Access-Control-Allow-Credentials: true
+//                                  Content-Type: text/plain
+//                Response Body: 
+
+// TCP/IP Socket Connection
+
+// TCP- Transmission Control Protocol/IP- Internet Protocol
+//                                                 Protocol= set of rules and regulation for data transfer between devices
+
+// How client sends data to server? - through data chuncks
+
+
+
