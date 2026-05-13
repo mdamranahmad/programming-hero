ea2ec03c591d2df3a9e3082c7a01236c49b8e187
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-14 00:57:21 +0600

    Reading a file from database using fs filesystem, usas of JSON for M06V04

--- Level 2 Course/Module 06 Node.js REST API Development and Server Architecture/src/controller/product-controller.ts
+++ Level 2 Course/Module 06 Node.js REST API Development and Server Architecture/src/controller/product-controller.ts
@@ -1,10 +1,11 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { readProduct } from "../services/product-service";
 
 export const productController = (req : IncomingMessage, res : ServerResponse) => {
     const url = req.url;
     const method = req.method;
 
-    const product = ;
+    const product = readProduct();
 
     url?.startsWith('/products') && method === 'GET' ?
         (res.writeHead(200, {'Content-Type' : 'application/json',}).
---
+++ Level 2 Course/Module 06 Node.js REST API Development and Server Architecture/src/services/product-service.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from "node:path";
+
+const filePath = path.join(process.cwd(), "./src/database/db.json");
+/*
+function path.join(...paths: string[]): string
+Join all arguments together and normalize the resulting path.
+*/
+
+export const readProduct = () => {
+    // console.log(filePath);
+    // fs is node file system to read and write file
+    const products = fs.readFileSync(filePath, 'utf8'); 
+    // fs.readFileSync- reads the file from the given path, utf8 encode the content into human readable form
+    // console.log(products.toString());
+    // console.log(products);
+    return JSON.parse(products);
+};
+
+/*
+The process.cwd() method returns the current working directory of the Node.js process.
+*/
