700d23b2ebc278b28442e3bc112b720343c58319
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-13 19:22:23 +0600

    Concepts of stream, buffers and modular systems for M05v08

---
+++ Level 2 Course/Module 05 Node.js Core Concepts Event Loop and Module/M05V08.ts
@@ -0,0 +1,15 @@
+//  Stream, Buffers and Modular Systems Deep Dive
+
+//  Stream  -   Slicing Data into managable chunks
+//  Buffer  -   storing chanks of data into a memory/ temporary storage
+//  Types of Stream -   Readable Stream
+//                  -   Wrirable Stream
+//                  -   Duplex Stream
+//                  -   Transform Stream
+
+//  IIFE    -   coinverting global scope variable into functional scope variable to reduce conflict
+
+//  Modular System  -   commonjs, esm module
+//      -   Local Modules
+//      -   Builtin Modules
+//      -   Third Party Modules
