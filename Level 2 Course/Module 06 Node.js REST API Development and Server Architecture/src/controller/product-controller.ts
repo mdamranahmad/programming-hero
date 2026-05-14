609d2be4a038e9bf3e2d8567c2be16a870e1cb01
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-14 23:45:24 +0600

    Concept of POST method, json parse, creating POST request using Postman, outside of project for M06V06

--- Level 2 Course/Module 06 Node.js REST API Development and Server Architecture/src/controller/product-controller.ts
+++ Level 2 Course/Module 06 Node.js REST API Development and Server Architecture/src/controller/product-controller.ts
@@ -1,6 +1,8 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../services/product-service";
 import type { IProduct } from "../types/product-types";
+import { json } from "node:stream/consumers";
+import { parseBody } from "../utility/parse-body";
 
 export const productController = async (req : IncomingMessage, res : ServerResponse) => {
     const url = req.url;
@@ -10,21 +12,52 @@ export const productController = async (req : IncomingMessage, res : ServerRespo
     const products = readProduct();
     const productId  = urlArr?.[urlArr?.length - 1];
     const product = products.find((p : IProduct) => p.id === productId);
+
+    // console.log("Request", req);
  
-    productId === 'products' && method === 'GET' ?
-        (res.writeHead(200, {'Content-Type' : 'application/json',}).
-        end(JSON.stringify({
-            message : 'This is product landing page.',
-            data: products,    
-        }))) :
-            method === 'GET' && product ?
-                (res.writeHead(200, {'content-type' : 'application/json'}))
-                    .end(JSON.stringify({
-                        message: `Product ${productId} is retrieved`,
-                        data: product,
-                    })) :
-                        (res.writeHead(200, {'content-type' : 'text/plain'}))
-                    .end(JSON.stringify({
-                        message: 'ERROR 404 !! Not Found',
-                    })) ;
+    // productId === 'products' && method === 'GET' ?
+    //     (res.writeHead(200, {'Content-Type' : 'application/json',}).
+    //     end(JSON.stringify({
+    //         message : 'This is product landing page.',
+    //         data: products,    
+    //     }))) :
+    //         method === 'GET' && product ?
+    //             (res.writeHead(200, {'content-type' : 'application/json'}))
+    //                 .end(JSON.stringify({
+    //                     message: `Product ${productId} is retrieved`,
+    //                     data: product,
+    //                 })) :
+    //                     (res.writeHead(200, {'content-type' : 'text/plain'}))
+    //                 .end(JSON.stringify({
+    //                     message: 'ERROR 404 !! Not Found',
+    //                 })) ;
+
+    if (method === 'GET') {
+        productId === 'products' ?
+            res.writeHead(200, {"content-type" : 'application/json'})
+                .end(JSON.stringify({
+                    message: "This is product landing page",
+                    data: products,
+                })) :
+                    product ?
+                        res.writeHead(200, {"content-type": "application/json"})
+                            .end(JSON.stringify({
+                                message: `Product ${productId} is retrived!`,
+                                data: product,
+                            })) :
+                                res.writeHead(200, {"content-type": "application/json"})
+                                    .end(JSON.stringify({
+                                        message: "ERROR 404!! Not Found!!!"
+                                    }));
+    } else if (method === 'POST') {
+        if (productId === 'products') {
+            const body = await parseBody(req);
+            console.log(body);
+            res.writeHead(200, {"content-type": "application/json"})
+                .end(JSON.stringify({
+                    message: `Product created succesfully!`,
+                }))
+
+        }
+    };
 };
