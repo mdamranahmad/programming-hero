436cbe0ee7e2bd13d0d193e8410f04b2f54a5c5f
ahmad-v0 <amranahmad.md@gmail.com>
2026-05-14 21:49:43 +0600

    Building GET methods operation, concept of returning individual product dynamically for M06V05

--- Level 2 Course/Module 06 Node.js REST API Development and Server Architecture/src/controller/product-controller.ts
+++ Level 2 Course/Module 06 Node.js REST API Development and Server Architecture/src/controller/product-controller.ts
@@ -1,17 +1,30 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../services/product-service";
+import type { IProduct } from "../types/product-types";
 
-export const productController = (req : IncomingMessage, res : ServerResponse) => {
+export const productController = async (req : IncomingMessage, res : ServerResponse) => {
     const url = req.url;
+    const urlArr : string[] | undefined = url?.split('/').filter(Boolean);
     const method = req.method;
 
-    const product = readProduct();
-
-    url?.startsWith('/products') && method === 'GET' ?
+    const products = readProduct();
+    const productId  = urlArr?.[urlArr?.length - 1];
+    const product = products.find((p : IProduct) => p.id === productId);
+ 
+    productId === 'products' && method === 'GET' ?
         (res.writeHead(200, {'Content-Type' : 'application/json',}).
         end(JSON.stringify({
             message : 'This is product landing page.',
-            data: product,    
+            data: products,    
         }))) :
-            null ;
+            method === 'GET' && product ?
+                (res.writeHead(200, {'content-type' : 'application/json'}))
+                    .end(JSON.stringify({
+                        message: `Product ${productId} is retrieved`,
+                        data: product,
+                    })) :
+                        (res.writeHead(200, {'content-type' : 'text/plain'}))
+                    .end(JSON.stringify({
+                        message: 'ERROR 404 !! Not Found',
+                    })) ;
 };
